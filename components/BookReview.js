9561b24238efd37d9bea0019f2eabaf1458a128b
ganglyimp <titiski15@gmail.com>
2023-04-13 23:15:07 -0400

    Improved styling on Bell Books page.

--- components/BookReview.js
+++ components/BookReview.js
@@ -27,35 +27,39 @@ class BookReview extends HTMLElement {
 				</div>
 
 				<dialog id="bookModal">
-					<form method="dialog">
-						<button>
-							<!--X Icon-->
-							<svg xmlns="http://www.w3.org/2000/svg" width="3em" height="3em" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
-  								<path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-							</svg>
-						</button>
-					</form>
+					<div class="fixed">
+						<form method="dialog">
+							<button>
+								<!--X Icon-->
+								<svg xmlns="http://www.w3.org/2000/svg" width="3em" height="3em" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+  									<path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+								</svg>
+							</button>
+						</form>
+					</div>
 
-					<header>
-						<div class="book-cover-preview"></div>
+					<div class="scrollable">
+						<header>
+							<div class="book-cover-preview"></div>
 
-						<div>
-							<h3 class="book-review-title"></h3>
-							<p>${bookCredits}</p>
+							<div>
+								<h3 class="book-review-title"></h3>
+								<p class="book-credits">${bookCredits}</p>
 
-							<br/>
+								<br/>
 
-							<p>${this.publisherInfo}</p>
-							<p>ISBN: ${this.isbn}</p>
-							<p>${this.materialInfo}</p>
-						</div>
-					</header>
+								<p>${this.publisherInfo}</p>
+								<p>ISBN: ${this.isbn}</p>
+								<p>${this.materialInfo}</p>
+							</div>
+						</header>
 
-					<blockquote class="book-review-content">
-						<slot name="review-text">
-							No review was provided for this book.
-						</slot>
-					</blockquote>
+						<blockquote class="book-review-content">
+							<slot name="review-text">
+								No review was provided for this book.
+							</slot>
+						</blockquote>
+					</div>
 				</dialog>
 			</article>`;
 		
@@ -116,18 +120,32 @@ class BookReview extends HTMLElement {
 
 					#bookModalToggle {
 						width: 50%;
+						position: relative;
 						margin: auto;
 						padding: 0;
 						background-color: transparent;
-						border: 3px solid var(--text-color);
+						border: 2px solid var(--text-color);
 						border-radius: 10px;
-						overflow: hidden;
 						cursor: pointer;
-						transition: all 0.1s;
+						transition: all 0.5s;
 					}
-					#bookModalToggle:hover {
+
+					#bookModalToggle:hover::before, #bookModalToggle:hover::after {
+						content: "";
+						position: absolute;
+						top: -10px;
+						left: -10px;
+						right: -10px;
+						bottom: -10px;
 						border: 5px solid var(--accent-secondary);
+						transition: all 0.5s;
+						animation: bookHover 3s infinite linear;
+						border-radius: 10px;
 					}
+					#bookModalToggle:hover::after {
+						animation: bookHover 3s infinite -1.5s;
+					}
+
 					#bookModalToggle.cover-missing {
 						min-height: 350px;
 					}
@@ -135,6 +153,7 @@ class BookReview extends HTMLElement {
 						#bookModalToggle img {
 							width: 100%;
 							height: 100%;
+							border-radius: 10px;
 						}
 
 						#bookModalToggle div.book-cover {
@@ -146,29 +165,44 @@ class BookReview extends HTMLElement {
 					width: 90%;
 					border-radius: 10px;
 					overscroll-behavior: none;
+					border: 2px solid var(--border-color);
 				}
-				
-					#bookModal button {
-						position: absolute;
-						top: 15px;
-						right: 15px;
-						border-radius: 50%;
-						border: 3px solid var(--text-color);
-						background-color: var(--main-bg-color);
-						cursor: pointer;
-						transition: all 0.3s ease-in-out;
-					}
-					#bookModal button:hover {
-						color: var(--accent-secondary);
-						border-color: var(--accent-secondary);
-					}
 
-						#bookModal button .bi {
+				#bookModal::backdrop {
+					background-color: rgba(0, 0, 0, 0.5);
+				}
+
+					#bookModal .fixed {
+						position: sticky;
+						top: 0;
+						z-index: 1;
+					}
+					
+						#bookModal .fixed button {
+							position: absolute;
+							top: 0;
+							right: 0;
+							border-radius: 100%;
+							border: 3px solid var(--text-color);
+							background-color: var(--main-bg-color);
+							cursor: pointer;
 							transition: all 0.3s ease-in-out;
 						}
-						#bookModal button .bi:hover {
-							transform: rotate(90deg);
+						#bookModal .fixed button:hover {
+							color: var(--accent-secondary);
+							border-color: var(--accent-secondary);
 						}
+
+							#bookModal .fixed button .bi {
+								transition: all 0.3s ease-in-out;
+							}
+							#bookModal .fixed button:hover .bi {
+								transform: rotate(90deg);
+							}
+					
+					#bookModal .scrollable {
+						overflow-y: scroll;
+					}		
 
 					#bookModal header {
 						display: flex;
@@ -189,6 +223,7 @@ class BookReview extends HTMLElement {
 							#bookModal header .book-cover-preview img {
 								width: 50%;
 								border-radius: 10px;
+								border: 1px solid var(--border-color);
 							}
 					
 					#bookModal blockquote {
@@ -208,6 +243,43 @@ class BookReview extends HTMLElement {
 						font-weight: bold;
 					}
 
+				@keyframes bookHover {
+					0%, 
+					100% {
+					  clip-path: inset(0 0 98% 0);
+					}
+					25% {
+					  clip-path: inset(0 98% 0 0);
+					}
+					50% {
+					  clip-path: inset(98% 0 0 0);
+					}
+					75% {
+					  clip-path: inset(0 0 0 98%);
+					}
+				}
+			
+			@media (max-width: 800px) {
+				#bookModal {
+					width: 100%;
+					height: 100%;
+					border-radius: unset;
+				}
+
+					#bookModal .fixed {
+						position: fixed;
+						top: 10px;
+						right: 10px;
+					}
+
+					#bookModal .book-review-title, #bookModal .book-credits {
+						text-align: center;
+					}
+
+					#bookModal blockquote {
+						margin: 0;
+					}
+			}
 		`;
 
 		shadowRoot.appendChild(style);
