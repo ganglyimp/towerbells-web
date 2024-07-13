63e44b1808d5ebc024e188d5552848a7e0210166
ganglyimp <titiski15@gmail.com>
2024-07-13 00:22:38 -0400

    Cleaned up code. Converted all links to root-based links. Minor style cleanup.

--- components/NavBar.js
+++ components/NavBar.js
@@ -3,7 +3,6 @@ class NavBar extends HTMLElement {
 		super();
 		this.attachShadow({ mode: 'open' });
 
-		this.pagePath = this.getAttribute('pagePath');
 		this.active = this.getAttribute('active');
 		this.languageData = [];
 
@@ -14,7 +13,7 @@ class NavBar extends HTMLElement {
 		const { shadowRoot } = this;
 
 		// Grab external JSON data
-		let jsonResponse = await fetch(`${this.pagePath}/../data/languages.json`);
+		let jsonResponse = await fetch(`/data/languages.json`);
 		this.languageData = await jsonResponse.json();
 
 		shadowRoot.innerHTML = this.getBaseHtml();
@@ -125,18 +124,18 @@ class NavBar extends HTMLElement {
 
 			<div class="nav-content collapsed">
 				<ul>
-					<li id="home"><a href="${this.pagePath}/../index.html">Home</a></li>
-					<li id="introduction"><a href="${this.pagePath}/Introduction/TowerBells.html">Introduction</a></li>
+					<li id="home"><a href="/index.html">Home</a></li>
+					<li id="introduction"><a href="/pages/Introduction/TowerBells.html">Introduction</a></li>
 					<li id="find-carillons"><a href="./data/Data_Top.html">Find Carillons</a></li>
-					<li id="books"><a href="${this.pagePath}/BellBooks/BellBooks.html">Books</a></li>
-					<li id="whats-new"><a href="${this.pagePath}/WhatsNew/WhatIsNew.html">What's New</a></li>
-					<li id="subscribe"><a href="${this.pagePath}/Subscribe.html">Subscribe</a></li>
-					<li id="web-history"><a href="${this.pagePath}/WebsiteHistory.html">Website History</a></li>
+					<li id="books"><a href="/pages/BellBooks/BellBooks.html">Books</a></li>
+					<li id="whats-new"><a href="/pages/WhatsNew/WhatIsNew.html">What's New</a></li>
+					<li id="subscribe"><a href="/pages/Subscribe.html">Subscribe</a></li>
+					<li id="web-history"><a href="/pages/WebsiteHistory.html">Website History</a></li>
 				</ul>
 
 				<div class="nav-actions">
 					<!-- Search Icon -->
-					<a href="${this.pagePath}/SearchTowerBells.html" title="Search">
+					<a href="/pages/SearchTowerBells.html" title="Search">
 						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
 							<path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
 						</svg>
