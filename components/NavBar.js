b9ee3b98594abab9469e2b65bdd7a5534a58d3ad
ganglyimp <titiski15@gmail.com>
2024-07-14 17:17:34 -0400

    fix(links): Updated all root-relative links to point to gh-pages project folder

--- components/NavBar.js
+++ components/NavBar.js
@@ -108,7 +108,7 @@ class NavBar extends HTMLElement {
 		<nav class="nav-bar">
 			<div class="nav-collapsed">
 				<div class="logo-and-title">
-					<a href="/index.html">
+					<a href="/towerbells-web/index.html">
 						<img src="/images/logos/favicon_original.png" alt="Bell Logo" width="50px" />
 					</a>
 					<b>Tower Bells</b>
@@ -129,18 +129,18 @@ class NavBar extends HTMLElement {
 
 			<div class="nav-content collapsed">
 				<ul>
-					<li id="home"><a href="/index.html">Home</a></li>
-					<li id="introduction"><a href="/pages/Introduction/TowerBells.html">Introduction</a></li>
-					<li id="find-carillons"><a href="/data/Data_Top.html">Find Carillons</a></li>
-					<li id="books"><a href="/pages/BellBooks/BellBooks.html">Books</a></li>
-					<li id="whats-new"><a href="/pages/WhatsNew/WhatIsNew.html">What's New</a></li>
-					<li id="subscribe"><a href="/pages/Subscribe.html">Subscribe</a></li>
-					<li id="web-history"><a href="/pages/WebsiteHistory.html">Website History</a></li>
+					<li id="home"><a href="/towerbells-web/index.html">Home</a></li>
+					<li id="introduction"><a href="/towerbells-web/pages/Introduction/TowerBells.html">Introduction</a></li>
+					<li id="find-carillons"><a href="/towerbells-web/data/Data_Top.html">Find Carillons</a></li>
+					<li id="books"><a href="/towerbells-web/pages/BellBooks/BellBooks.html">Books</a></li>
+					<li id="whats-new"><a href="/towerbells-web/pages/WhatsNew/WhatIsNew.html">What's New</a></li>
+					<li id="subscribe"><a href="/towerbells-web/pages/Subscribe.html">Subscribe</a></li>
+					<li id="web-history"><a href="/towerbells-web/pages/WebsiteHistory.html">Website History</a></li>
 				</ul>
 
 				<div class="nav-actions">
 					<!-- Search Icon -->
-					<a href="/pages/SearchTowerBells.html" title="Search">
+					<a href="/towerbells-web/pages/SearchTowerBells.html" title="Search">
 						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
 							<path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
 						</svg>
