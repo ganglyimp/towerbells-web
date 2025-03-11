fd783e920ff4d39e2ad2811b7218b967c84ac580
ganglyimp <titiski15@gmail.com>
2025-03-10 21:32:40 -0400

    style: Minor fixes to shrink some elements on pages

--- components/NavBar.js
+++ components/NavBar.js
@@ -436,7 +436,7 @@ class NavBar extends HTMLElement {
 					
 						nav dialog .body section div {
 							display: grid;
-							grid-template-columns: 1fr 1fr 1fr 1fr;
+							grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
 						}
 
 							nav dialog .body section div button {
